4fbea034e7a10deaf7c2bac0945395753daf972b
GrahamParvinkarimi <gparvinkarimi@mdy97r3xhvj.myfiosgateway.com>
2024-09-17 15:35:32 -0400

    -Allowed the URL to be nullable, can edit the root node now

--- src/main/java/org/jetbrains/plugins/template/MyDialog.kt
+++ src/main/java/org/jetbrains/plugins/template/MyDialog.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.layout.ComponentPredicate
 import com.intellij.ui.layout.not
 import org.jetbrains.plugins.template.domain.MyMenuItem
 import javax.swing.JComponent
@@ -12,7 +13,7 @@ import javax.swing.JComponent
 /**
  * Maintenance dialog.
  */
-class MyDialog(val myMenuItem: MyMenuItem, val onSubmit: (MyMenuItem) -> Unit): DialogWrapper(true) {
+class MyDialog(val myMenuItem: MyMenuItem, val onSubmit: (MyMenuItem) -> Unit) : DialogWrapper(true) {
 
     private lateinit var _title: Cell<JBTextField>
     private lateinit var _url: Cell<JBTextField>
@@ -31,18 +32,29 @@ class MyDialog(val myMenuItem: MyMenuItem, val onSubmit: (MyMenuItem) -> Unit): 
         title.enabledIf(_divider.selected.not())
         url.enabledIf(_divider.selected.not())
 
-        _title.component.document.addDocumentListener(textListener(_title.component))
-        _url.component.document.addDocumentListener(textListener(_url.component))
+        _title.component.document.addDocumentListener(textListener(_title.component, _divider.selected))
+        _url.component.document
+
+        _divider.component.addActionListener {
+            // Enable the OK button if
+            if (_divider.component.isSelected) {
+                isOKActionEnabled = true
+            } else {
+                isOKActionEnabled = _title.component.text.isNotEmpty()
+            }
+        }
     }
 
-    // Enable the OK button when the user enters text
-    private fun textListener(component: JBTextField) = object : javax.swing.event.DocumentListener {
+    // Enable the OK button when the user enters text in the label field (if this isn't a divider)
+    private fun textListener(component: JBTextField, isDivider: ComponentPredicate) = object : javax.swing.event.DocumentListener {
         override fun insertUpdate(e: javax.swing.event.DocumentEvent) {
             isOKActionEnabled = component.text.isNotEmpty()
         }
+
         override fun removeUpdate(e: javax.swing.event.DocumentEvent) {
             isOKActionEnabled = component.text.isNotEmpty()
         }
+
         override fun changedUpdate(e: javax.swing.event.DocumentEvent) {
             isOKActionEnabled = component.text.isNotEmpty()
         }
@@ -51,7 +63,7 @@ class MyDialog(val myMenuItem: MyMenuItem, val onSubmit: (MyMenuItem) -> Unit): 
     override fun doValidate(): ValidationInfo? {
         if (_divider.component.isSelected) return null
         if (_title.component.text.isBlank()) return ValidationInfo("Title is required!")
-        if (!_url.component.text.startsWith("http")) return ValidationInfo("URL must include prefix!")
+        if (_url.component.text.isNotEmpty() && !_url.component.text.startsWith("http")) return ValidationInfo("URL must include prefix!")
         return null
     }
 
